75f98a61d2f4abd70b1e7a32e433972cb43e8d76
Luis <luis.here.j@gmail.com>
2021-02-10 21:10:45 +0100

    Hasta las provincias

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,6 @@
-import { sumar, restar, multiplicar, dividir } from '../index.js';
+import { sumar, restar, multiplicar, dividir, isFalse, isNull, isTrue, isUndefined, arrDias, arrProvincias } from '../index.js';
+
+//test unitarios
 
 describe('Operaciones matemáticas', () => {
     test('Realizamos la suma', () => {
@@ -13,4 +15,66 @@ describe('Operaciones matemáticas', () => {
     test('Realizamos la division', () => {
         expect(dividir(1,1)).toBe(1);
     });
+});
+
+//matchers para probar los diferentes valores
+
+describe('Common matchers', () => {
+    const datos = {
+        nombre: 'Persona 1',
+        edad: 10
+    }
+    const datos2 = {
+        nombre: 'Persona 1',
+        edad: 10
+    }
+    test('Comprobamos que los objectos son iguales', () => {
+        expect(datos).toEqual(datos2);
+    });
+});
+
+//comprobar si el valor es menor, igual o superior
+
+describe ('Matchers numéricos', () => {
+    test('Resultado menor que...', () => {
+        expect(restar(5,3)).toBeLessThan(3);
+    });
+    test('Resultado menor o igual que...', () => {
+        expect(restar(5,3)).toBeLessThanOrEqual(2);
+    });
+    test('Resultado mayor o igual que...', () => {
+        expect(multiplicar(2,5)).toBeGreaterThanOrEqual(10);
+    });
+    test('Resultado mayor que...', () => {
+        expect(sumar(5,5)).toBeGreaterThan(9);
+    });
+}); 
+
+// test para Boolean, undefined o null
+
+describe ('Matchers boolean, undefined o null', () => {
+    test('Resultado true', () => {
+        expect(isTrue()).toBeTruthy();
+    });
+    test('REsultado false', () => {
+        expect(isFalse()).toBeFalsy();
+    });
+    test('Resultado undefined', () => {
+        expect(isUndefined()).toBeUndefined();
+    });
+    test('REsultado null', () => {
+        expect(isNull()).toBeNull();
+    });
+});
+
+describe('Matchers Arrays', () => { 
+    test('Madrid existe en el array', () => {
+        expect(arrProvincias()).toContain('Madrid');
+    });
+    test('Guadalajara no existe en el array', () => {
+        expect(arrProvincias()).not.toContain('Guadalajara');
+    })
+    test('El array semana tiene 9 elementos', () => {
+        expect(arrProvincias()).toHaveLength(9);
+    })
 });
